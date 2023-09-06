35dc0d5864c0083751d9c65f5f2f8a12840e9bfa
Nicolas Cayez <130738851+NicolasCayez@users.noreply.github.com>
2023-09-06 15:11:11 +0200

    exercice arrays

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 console.log('Bienvenue dans Javascript');
 
 /**
- * **************************************
- * 1-SETUP : DEFER
- * **************************************
- ** Antérieur à la vague HTML5, l'attribut defer existait déjà dans les "anciennes" versions
- ** d'Internet Explorer. Il signifie que le navigateur 
- *! peut charger le(s) script(s) en parallèle, sans stopper le rendu de la page HTML.
- *! Contrairement à async, l'ordre d'exécution des scripts est préservé,
- *! en fonction de leur apparition dans le code source HTML. 
- *? Il est par ailleurs reporté à la fin du parsing du DOM (avant l'événement DOMContentLoaded).
- * De nos jours, cet attribut présente moins d'intérêt car les navigateurs
- * disposent par défaut de techniques internes pour télécharger les 
- * ressources en parallèle sans nécessairement attendre les autres.
- */
+* **************************************
+* 1-SETUP : DEFER
+* **************************************
+** Antérieur à la vague HTML5, l'attribut defer existait déjà dans les "anciennes" versions
+** d'Internet Explorer. Il signifie que le navigateur 
+*! peut charger le(s) script(s) en parallèle, sans stopper le rendu de la page HTML.
+*! Contrairement à async, l'ordre d'exécution des scripts est préservé,
+*! en fonction de leur apparition dans le code source HTML. 
+*? Il est par ailleurs reporté à la fin du parsing du DOM (avant l'événement DOMContentLoaded).
+* De nos jours, cet attribut présente moins d'intérêt car les navigateurs
+* disposent par défaut de techniques internes pour télécharger les 
+* ressources en parallèle sans nécessairement attendre les autres.
+*/
 
 /**
- * **************************************
- * SETUP : ASYNC
- * **************************************
- * *Nouveau venu dans HTML5, async signifie que le script pourra être exécuté de façon asynchrone,
- ** dès qu'il sera disponible (téléchargé). Cela signifie aussi que le navigateur 
- ** n'attendra pas de suivre un ordre particulier si plusieurs balises <script> sont présentes,
- ** et ne bloquera pas le chargement du reste des ressources, notamment la page HTML. 
- *? L'exécution aura lieu avant l'événement load lancé sur window 
- *? et ne sera valable que pour les scripts externes au document, 
- *? c'est-à-dire ceux dont l'attribut src mentionne l'adresse.
- *? Ce comportement est bien pratique pour gagner en temps de chargement,
- *! il faut cependant l'utiliser avec prudence : si l'ordre n'est pas respecté,
- *! un fichier exécuté de façon asynchrone ne pourra attendre le chargement d'un précédent,
- *! par exemple s'il en utilise des fonctions voire un framework.
- *! Il ne faudra pas non plus compter appeler document.write() pour écrire dans le document HTML
- *! puisqu'il sera impossible de savoir à quel moment les actions seront déclenchées.
- */ 
+* **************************************
+* SETUP : ASYNC
+* **************************************
+* *Nouveau venu dans HTML5, async signifie que le script pourra être exécuté de façon asynchrone,
+** dès qu'il sera disponible (téléchargé). Cela signifie aussi que le navigateur 
+** n'attendra pas de suivre un ordre particulier si plusieurs balises <script> sont présentes,
+** et ne bloquera pas le chargement du reste des ressources, notamment la page HTML. 
+*? L'exécution aura lieu avant l'événement load lancé sur window 
+*? et ne sera valable que pour les scripts externes au document, 
+*? c'est-à-dire ceux dont l'attribut src mentionne l'adresse.
+*? Ce comportement est bien pratique pour gagner en temps de chargement,
+*! il faut cependant l'utiliser avec prudence : si l'ordre n'est pas respecté,
+*! un fichier exécuté de façon asynchrone ne pourra attendre le chargement d'un précédent,
+*! par exemple s'il en utilise des fonctions voire un framework.
+*! Il ne faudra pas non plus compter appeler document.write() pour écrire dans le document HTML
+*! puisqu'il sera impossible de savoir à quel moment les actions seront déclenchées.
+*/ 
 
 
 //  EXERCICE VARIABLES
@@ -57,11 +57,11 @@ console.log(bouboule);
 let hello = function (texte) { console.log(texte)};
 hello(`hello world`);
 /**
- * **************************************
- * ! CORRECTION
- * 2-VARIABLES
- * **************************************
- */
+* **************************************
+* ! CORRECTION
+* 2-VARIABLES
+* **************************************
+*/
 //! On déclare une variable avec let ou const (ou var dans les anciennes versions de JS)
 let maVariable;
 //! On assigne une valeur à une variable avec le signe =
@@ -93,10 +93,10 @@ console.log(unObjet);
 console.log(unObjet.propriete2);
 
 /**
- * **************************************
- * 3-CALCULS
- * **************************************
- */
+* **************************************
+* 3-CALCULS
+* **************************************
+*/
 console.log(42*675);
 let unChiffre = 9;
 let unNombre = 33;
@@ -136,10 +136,10 @@ console.log(unCompteur);
 console.log(unChiffre**9);
 
 /**
- * **************************************
- * EXERCICE PHRASE
- * **************************************
- */
+* **************************************
+* EXERCICE PHRASE
+* **************************************
+*/
 
 let nom = `Donatello`;
 let date = `05 Septembre`;
@@ -156,10 +156,10 @@ let sumUpOrderPhrase3 = "Le "+date+"\nBonjour "+ nom+"\nMerci d'avoir commandé 
 console.log(sumUpOrderPhrase3);
 
 /**
- * **************************************
- * EXERCICE ARRAYS
- * **************************************
- */
+* **************************************
+* EXERCICE ARRAYS
+* **************************************
+*/
 //TODO: Créer une variable pour un nom
 let nom1 = `John`;
 //TODO: Créer une variable pour un âge
